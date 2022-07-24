e42398ca5b5eaae2a79c5ab8a88510158a4f0830
AlekseyGolikov <golikov.aleksey.1987@yandex.ru>
2022-07-24 18:10:48 +0300

    nrf24_transmitter

---
+++ NRF24_Transmitter/MDK-ARM/RTE/_NRF24_Transmitter/RTE_Components.h
@@ -0,0 +1,21 @@
+
+/*
+ * Auto generated Run-Time-Environment Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'NRF24_Transmitter' 
+ * Target:  'NRF24_Transmitter' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+/*
+ * Define the Device Header File: 
+ */
+#define CMSIS_device_header "stm32f10x.h"
+
+
+
+#endif /* RTE_COMPONENTS_H */
